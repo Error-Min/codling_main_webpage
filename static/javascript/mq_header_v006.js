2959de20486f96d1f34378730da53e2f617f8fd8
Error-Min <smgm030@gmail.com>
2023-10-02 23:03:39 +0900

    submenu

---
+++ static/javascript/mq_header_v006.js
@@ -0,0 +1,22 @@
+// jQuery 문서 준비 이벤트
+$(document).ready(function(){
+
+    // 모바일 환경인지 확인하는 함수
+    function isMobile() {
+        return window.innerWidth <= 1024;
+    }
+
+    // 메뉴 버튼 클릭 이벤트
+    $('.menu_btn').click(function(){
+        // 모바일 환경일 때만 동작
+        if (isMobile()) {
+            // 메뉴 버튼의 클래스 토글
+            $('.menu_btn').toggleClass('lijo');
+            // gnb의 클래스 토글
+            $('.gnb').toggleClass('surya');
+            // 초기화: 서브메뉴 스타일 제거
+            $('.sub').removeAttr('style');  
+        }
+    });
+    
+});
